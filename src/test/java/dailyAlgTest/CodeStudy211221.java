da8ea2559774d2ee267b5e592fecfb97401ab7d4
minhyeok_lee <minhyeok_lee@tmax.co.kr>
2021-12-26 23:33:51 +0900

    아 문제 안풀리네

--- src/test/java/dailyAlgTest/CodeStudy211221.java
+++ src/test/java/dailyAlgTest/CodeStudy211221.java
@@ -1,5 +1,7 @@
 package dailyAlgTest;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 
 public class CodeStudy211221 {
@@ -11,7 +13,69 @@ public class CodeStudy211221 {
 	@Test
 	public void Test1() {
 		
+		//String digits = nDigitConverter(6 - 1);
+		assertEquals("1", nDigitConverter(1));
+		
+		assertEquals("2", nDigitConverter(2));
+		
+		assertEquals("11", nDigitConverter(4));
+		
+		//assertEquals("4", nDigitConverter(2));
+		//assertEquals("21", nDigitConverter(6));
+		//assertEquals("14", nDigitConverter(5));
+		
+		//assertEquals("21", nDigitConverter(7));
+		//assertEquals("22", nDigitConverter(8));
+		//assertEquals("21", nDigitConverter(7));
+		
 	}
+	
+	public String nDigitConverter(int n) {
+		
+		int digitChar[] = new int[] {0, 1, 2, 4}; //각각 나머지 1일때 1, 2일때 0, 0일때 4.
+		
+		StringBuffer digits = new StringBuffer();
+		
+		int origin = n;
+		int count = 0;
+		int pownum = 0;
+		int current = 0;
+		int mod = 1;
+		
+		if(origin < 4 && origin >= 0)
+			return Integer.toString(digitChar[origin]);
+		
+		for(int i = 1; origin > 0; i++) {
+			mod = origin % 4;
+			System.out.println("origin = " + origin + ", mod = " + mod );
+			origin /= 4;
+			
+			digits.insert(0, Integer.toString(digitChar[mod]));
+			//digits.append(Integer.toString(digitChar[mod]));
+			System.out.println(" digit : " + digits.toString());
+		}
+		
+		
+		return digits.toString();
+	}
+	
+	/// 31 을 2로 나누어 생각한다
+	// LSB 부터.
+	
+	// 30 + 1, 28 + 2 + 1// 24 + 4 + 2+ 1
+	//
+	
+	//6->
+	
+	//5-> 14
+	// 몫 1, 나머지 .
+	//6-> 21
+	// 9
+	// 몫 2, 나머지 0
+	
+	//몫 2, 나머지 0 그래서 14.
+	//몫 2, 나머지 1
+	
 	
 	
 }
---
+++ src/test/java/dailyAlgTest/CodeStudy211226.java
@@ -0,0 +1,102 @@
+package dailyAlgTest;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class CodeStudy211226 {
+	
+	// https://programmers.co.kr/learn/courses/30/lessons/12973
+	
+	@Test
+	public void Test1() {
+		String s = "baabaa";
+		assertEquals(1, Solution2(s));
+		
+	}
+	
+	
+	@Test
+	public void Test2() {
+		String s = "cdcd";
+		assertEquals(0, Solution1(s));
+	}
+	
+	
+	public int Solution2(String s) {
+		
+		String result = "";
+		
+		result = returnStrs(s);
+		
+		if(result.length() == 0) return 1;
+		
+		return 0;
+	}
+	
+	public String returnStrs(String s) {
+		
+		System.out.println("string s = " + s);
+		int j = 0;
+		
+		for(int i = 0; i< s.length() -1; i++) {
+			
+			if(s.charAt(i) == s.charAt(i+1)){
+
+				int found = 0;
+				for(j = 0; j <= i && i + j + 1 < s.length(); j++) {
+					if(s.charAt(i - j) == s.charAt(i + 1 + j)) {
+						// 대칭성 검사를 하면 좀 나아지지 않을까?
+						found = 1;
+						continue;
+					}
+					
+					
+				}
+				if(j != 0 && found != 0 && i > j) {
+					return returnStrs(  returnStrs(s.substring(0, i - j) ) + returnStrs(s.substring(i+ 2 + j, s.length())) );
+				}
+				System.out.println("i = "+ i);
+				System.out.println("ret substring : ["+ s.substring(0, i) + "], [" +s.substring(i+2, s.length())+"]" );
+				return  returnStrs(  s.substring(0, i) + s.substring(i+2, s.length()) );
+				//return  returnStrs(  returnStrs(s.substring(0, i) ) + returnStrs(s.substring(i+2, s.length())) );
+			}
+			
+		}
+		return s;
+	}
+	
+	 
+	
+	public int Solution1(String s) {
+		
+		StringBuffer buf = new StringBuffer(s);
+		int i = 0;
+		
+		String before = s;
+		String after = "";
+		
+		while(!before.contentEquals(after)) {
+			before = buf.toString();
+			
+			i = 0;
+			while(i < buf.length() - 1) {
+				System.out.println(" parsing "+ buf.toString() + ",i ="+ i);
+				if (buf.charAt(i) == buf.charAt(i + 1)) {
+					System.out.println("before del : " + buf.toString() + ",length = " + buf.length());
+					buf.deleteCharAt(i+1);
+					buf.deleteCharAt(i);
+					System.out.println("after del : " + buf.toString() + ",length = " + buf.length());
+					//continue;
+				}
+				i++;
+			}
+			after = buf.toString();
+		}
+		
+		if (buf.toString().length() == 0) return 1;
+		
+		return 0;
+	}
+
+}

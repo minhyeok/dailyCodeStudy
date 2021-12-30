8a01ff1f583cc2c5f0f71608f8aec7cd6adfb21c
minhyeok_lee <into_arena@naver.com>
2021-12-31 01:07:09 +0900

    메일주소 변경 후 재커밋

---
+++ src/test/java/dailyAlgTest/CodeStudy211227.java
@@ -0,0 +1,28 @@
+package dailyAlgTest;
+
+import static org.junit.Assert.assertArrayEquals;
+
+import org.junit.Test;
+
+public class CodeStudy211227 {
+	
+	@Test
+	public void Test1() {
+		int [] progress = new int[] {93,30,55};
+		int [] speeds = new int[] {1,30,5};
+		
+		int[] answer = solution(progress, speeds);
+		int[] expects = new int[] {2,1};
+		
+		assertArrayEquals(expects,answer);
+	}
+	
+	public int[] solution(int[] progress, int[] speeds) {
+		int[] answer= {};
+		
+		
+		
+		return answer;
+	}
+
+}
